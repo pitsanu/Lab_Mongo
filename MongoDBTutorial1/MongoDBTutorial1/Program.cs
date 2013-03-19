561fe76bbc6fd88d1e359c7d629e00a2743e9100
Pitsanu-T <pitsanu.t@carpass.co.th>
2013-03-19 10:07:06 +0700

    1. Create solution 2. Add common libraly Signed-off-by: Pitsanu-T <pitsanu.t@carpass.co.th>

---
+++ MongoDBTutorial1/MongoDBTutorial1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDBTutorial1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "mongodb://localhost";
+
+        }
+    }
+}
