ffd79afc0e5fef7885181c9abfcf8e8eb29dc6e3
Pitsanu-T <pitsanu.t@carpass.co.th>
2013-03-21 14:57:49 +0700

    Add FirstAttemptAtNoSQLTutorial.csproj
    Signed-off-by: Pitsanu-T <pitsanu.t@carpass.co.th>

---
+++ FirstAttemptAtNoSQLTutorial/FirstAttemptAtNoSQLTutorial/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Mongo
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace FirstAttemptAtNoSQLTutorial
+{
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MongoServer server = MongoServer.Create();            
+
+            MongoClient client=new MongoClient();
+            MongoServer server = client.GetServer();
+            MongoDatabase db = server.GetDatabase("myTestDB");
+
+            using (server.RequestStart(db))
+            {
+                var col = db.GetCollection("employees");
+
+                //col.Insert(new BsonDocument { { "name", "John" }, { "last", "Smith" } });
+                //col.Insert(new BsonDocument { { "name", "Jane" }, { "last", "Smith" } });
+
+                var cursor = col.FindAll();
+                foreach (var item in cursor)
+                {
+                    string name = (string)item["name"];
+                    string last = (string)item["last"];
+                    Console.WriteLine("{0} {1}", name, last);
+                }
+            }
+        }
+    }
+}
---
+++ FirstAttemptAtNoSQLTutorial/FirstAttemptAtNoSQLTutorial/TypedEntityProgram.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Mongo
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FirstAttemptAtNoSQLTutorial
+{
+    [BsonIgnoreExtraElements(true)]
+    class Person
+    {
+        public BsonObjectId _id { get; set; }
+        public string name { get; set; }
+        //public string mid { get; set; }
+        public string last { get; set; }        
+    }
+
+    class TypedEntityProgram
+    {
+        //Have field in db, but no have in class
+        static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient();
+            MongoServer server = client.GetServer();
+            MongoDatabase db = server.GetDatabase("myTestDB");
+
+            using (server.RequestStart(db))
+            {
+                var col = db.GetCollection<Person>("employees");
+
+                //col.Insert(new Person { name = "John", mid = "m.", last = "Terry" });
+
+                var cursor = col.FindAllAs<Person>();
+                foreach (var item in cursor)
+                {
+                    //Console.WriteLine("{0} {1} {2}", item.name, item.mid, item.last);
+                    Console.WriteLine("{0} {1}", item.name, item.last);
+                }
+            }
+        }
+    }
+}
